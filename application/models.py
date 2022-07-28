4dcc2262545bd0657c8258fb969a119959c72e41
River <rivermartin.256@gmail.com>
2022-07-28 22:13:55 +0200

    Added a model for sign language libraries and a model for signs, with a one to many relationship from libraries to signs.

--- application/models.py
+++ application/models.py
@@ -1,8 +1,24 @@
 from . import db
 
+
 class user(db.Model):
     __tablename__ = 'user_account'
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    user_name = db.Column(db.String(128), nullable = False)
-    password_hash = db.Column(db.String(128), nullable = False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
+
+class SignLanguageLibrary(db.Model):
+    __tablename__ = 'sign_language_library'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(256), nullable=False)
+    # One to many relationship between SignLanguageLibrary and Sign
+    signs = db.relationship('Sign', backref='sign_language_library')
+
+
+class Sign(db.Model):
+    __tablename__ = 'sign'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    meaning = db.Column(db.String(256), nullable=False)
+    image_url = db.Column(db.String(256), nullable=False)
+    library_id = db.Column(db.Integer, db.ForeignKey('sign_language_library.id'), nullable=False)
