a3787933bb794ae3967f5e6c8a78b56c39f81af6
River <rivermartin.256@gmail.com>
2022-07-29 23:28:21 +0200

    Added a function to serve images from the file system.

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 # Globally accessible libraries
 db = SQLAlchemy()
 
@@ -11,14 +10,11 @@ def init_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config')
-
+    app.add_url_rule('/library/image', endpoint='get_sign_image', build_only=True)
     # Initialize Plugins
     db.init_app(app)
-
     with app.app_context():
         # Include our Routes
         from application import routes
-
         db.create_all()
-
         return app
