0c5a3ba75c81e2f12caa500466d9e778abbef526
River <rivermartin.256@gmail.com>
2022-07-29 22:03:55 +0200

    Implemented a route for uploading libraries with images in a .zip file and a .csv linking images to sign meanings.

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
-from flask import current_app as app
-from .models import user
+from flask import current_app as app, Response, request
+from .models import user, SignLanguageLibrary, Sign
 from zipfile import ZipFile
 import csv
 
@@ -12,15 +12,22 @@ def home():
 @app.route('/library/upload', methods=['POST'])
 def upload_library():
     try:
+        # Request arguments and files
         sign_meanings = request.files['sign_meanings']
         zipped_images = request.files['zipped_images']
-        img_path = app.config['IMAGE_PATH'] + '/' + request.form.to_dict()['library_name']
+        lib_name = request.form.to_dict()['library_name']
+        # Path this library's images will be saved to
+        img_path = app.config['IMAGE_PATH'] + '/' + lib_name
+        lib = SignLanguageLibrary(title=lib_name)
+        db.session.add(lib)
         reader = csv.reader(sign_meanings, delimiter=',')
         zpfl = ZipFile(zipped_images.stream._file)
         for line in reader:
             image_file_name = line[0]
             sign_meaning = line[1]
             zpfl.extract(image_file_name, path=img_path)
-
+            sign = Sign(meaning=sign_meaning, image_url=img_path+image_file_name, library_id=lib.id)
+            db.session.add(sign)
     except KeyError:
-        print('missing some files')
+        return Response(status=400)
+    return Response(status=200)
