b48b167386ace0507a479d7c7dbadc1e51299677
River <rivermartin.256@gmail.com>
2022-07-29 17:54:59 +0200

    Cleaned up the code in image_recognition.py

--- application/image_recognition.py
+++ application/image_recognition.py
@@ -2,66 +2,42 @@ import numpy as np
 import cv2 as cv
 
 """
-Experimentation with opencv
+Experimentation with opencv.
+
+Preprocesses images by grayscaling and flattening them (so that we can work with 1D vectors when
+training and classifying). Classifies a sample image, 'A1234.jpg', using an implementation of the 
+K-NearestNeighbours algorithm.
+
+After some slight modifications to this code we should have a functioning classifier for the API.
 """
 
 
-def preprocess_images(images):
-    n, m, k = images.shape[:3]
-    d = m*k
-    greyscaled = np.zeros((n, d), dtype=np.uint8)
-    i = 0
-    for img in images:
-        # Convert to greyscale and flatten
-        temp = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        greyscaled[i, :] = temp.flatten()
-        i += 1
-    # greyscaled = np.array(greyscaled)
-    return greyscaled
+def read_and_preprocess_image(char):
+    # Get an image from the images associated with <char> letter of the alphabet.
+    filename = '../asl_alphabet_train/{0}/{0}1234.jpg'.format(char)
+    return grayscale_and_flatten(cv.imread(filename))
 
 
-def get_trained_knn(training_data, labels):
-    assert training_data.shape[0] == labels.shape[0]
-    knn = cv.ml.KNearest_create()
-    knn.train(training_data.astype(np.float32), cv.ml.ROW_SAMPLE, labels[:np.newaxis])
-    return knn
+def grayscale_and_flatten(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY).flatten()
 
 
-def test_classify():
+def test_simplest_classify():
+    # Alphabet just used to build filename for loading images from the directory with the sample
+    # data.
     ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # The name of the image that will be classified
     INITIAL_TEST_IMAGE = '../asl_alphabet_train/A/A1234.jpg'
+    data = np.array([im for im in map(read_and_preprocess_image, ALPHABET)], dtype=np.float32)
+    test_images = np.array([grayscale_and_flatten(cv.imread(INITIAL_TEST_IMAGE))], dtype=np.float32)
+    # Assuming each image is in it's own class (i.e A, B, C, etc..)
     labels = np.arange(len(ALPHABET), dtype=np.float32)
-    data = []
-    for i in range(len(ALPHABET)):
-        filename = '../asl_alphabet_train/{0}/{0}1234.jpg'.format(ALPHABET[i])
-        img = cv.imread(filename)
-        data += [img]
-    data = np.array(data)
-    data = preprocess_images(data)
-    data = data[:, :, np.newaxis]
-    knn = get_trained_knn(data, labels)
-    test_image = preprocess_images(np.array([cv.imread(INITIAL_TEST_IMAGE)]))[0]
-    test_images = test_image[np.newaxis, :, np.newaxis]
-    test_images = test_images.astype(np.float32)
+    knn = cv.ml.KNearest_create()
+    knn.train(data, cv.ml.ROW_SAMPLE, labels)
     retval, results, responses, dists = knn.findNearest(test_images, k=12)
+    # The test image is from the training data for the letter A, so we expect it to be classified
+    # accordingly.
     assert results[0] == labels[0]
-
-
-"""
-def test_preprocess_images():
-    INITIAL_TEST_IMAGE = '../asl_alphabet_train/A/A1234.jpg'
-    images = np.array([cv.imread(INITIAL_TEST_IMAGE)])
-    # Delete the RGB dimension
-    newShape = images.shape[:3]
-    # Receive matrix of flattened vectors
-    preprocessed = preprocess_images(images)
-    # Reshape the matrix
-    preprocessed = np.reshape(preprocessed, newShape)
-    cv.imshow('before', images[0])
-    cv.imshow('after', preprocessed[0])
-    cv.waitKey(0)
-    assert True
-"""
 
 
 if __name__ == '__main__':
