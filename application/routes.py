bb00b1c436eddfad5394766ed9f69e98b34c1088
River <rivermartin.256@gmail.com>
2022-07-30 00:19:43 +0200

    Added a function to the Sign model that returns a dictionary representation of the sign.

--- application/routes.py
+++ application/routes.py
@@ -21,6 +21,7 @@ def upload_library():
         img_path = app.config['IMAGE_PATH'] + '/' + lib_name
         lib = SignLanguageLibrary(title=lib_name)
         db.session.add(lib)
+        db.session.commit()
         reader = csv.reader(sign_meanings, delimiter=',')
         zpfl = ZipFile(zipped_images.stream._file)
         for line in reader:
@@ -29,18 +30,18 @@ def upload_library():
             zpfl.extract(image_file_name, path=img_path)
             sign = Sign(meaning=sign_meaning, image_url=image_file_name, library_id=lib.id)
             db.session.add(sign)
+        db.session.commit()
     except KeyError:
         return Response(status=400)
     return Response(status=200)
 
 
-@app.route('/library/signs/<library_name>', methods=['GET'])
+@app.route('/library/signs', methods=['GET'])
 def get_signs():
-    img_url_base = '/library/image/'
-    lib = SignLanguageLibrary.query.first_or_404(title=library_name)
-    # The image url will be incorrect and the elements of the list below are likely not formatted
-    # correctly. Plan to correct this soon
-    signs = [sign for sign in lib.signs]
+    library_name = request.args['library_name']
+    img_url_base = '/library/image'
+    lib = SignLanguageLibrary.query.filter_by(title=library_name).first_or_404()
+    signs = [sign.to_dict(img_url_base) for sign in lib.signs]
     return {'signs': signs}
 
 
