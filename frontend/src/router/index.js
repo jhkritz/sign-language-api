dd4c6fe201938e344ca8f24f4b91b9796d847560
Shabih, MH, Miss [24397644@sun.ac.za] <24397644@sun.ac.za>
2022-07-29 22:04:49 +0200

    functional user_interface

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LibraryView from '../views/LibraryView.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -8,25 +10,19 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-
-  {
-    path: '/libraries',
-    name: 'libraries',
-    component: LibraryView
-  },
-
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
   routes
 })
 
---
+++ user_interface/src/views/LibraryView.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app fluid class="head">
+    <div>
+      <v-row justify="center">
+        <v-col cols="12" sm="8">
+          <v-container>
+            <v-content class="content">
+              <h1 class="black--text">Find</h1>
+            </v-content>
+
+            <v-content class="cont1">
+              <v-btn
+                rounded
+                color="black"
+                dark
+                @click="goToLogin()"
+                class="mr-10 mt-15 mb-10"
+              >
+                LOGIN
+              </v-btn>
+            </v-content>
+
+            <v-content class="cont2">
+              <v-btn
+                rounded
+                color="black"
+                dark
+                @click="goToSignUp()"
+                class="mt-15"
+              >
+                SIGNUP
+              </v-btn>
+            </v-content>
+          </v-container>
+        </v-col>
+      </v-row>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  components: {},
+
+  methods: {
+    goToLogin() {
+      this.$router.push("/login");
+    },
+    goToSignUp() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
+ 
+<style scoped>
+.head {
+  width: 100vw;
+  height: 100vh;
+  background-image: url(./../assets/friends.gif);
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  overflow: hidden;
+}
+
+.content {
+  position: absolute;
+  top: 35%;
+  left: 25%;
+  transform: translateY(-50%);
+  z-index: 2;
+}
+
+.cont1 {
+  position: absolute;
+  top: 56%;
+  left: 25%;
+  transform: translateY(-50%);
+  z-index: 2;
+}
+
+.cont2 {
+  position: absolute;
+  top: 54%;
+  left: 68%;
+  transform: translateY(-50%);
+  z-index: 2;
+}
+</style>
