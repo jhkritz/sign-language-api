c2892600d01a1dac39bd2e33acaff409ebbab0b7
River <rivermartin.256@gmail.com>
2022-07-31 22:10:51 +0200

    Implemented a route that classifies a single image.

--- backend/config.py
+++ backend/config.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'GDtfDCFYjD'
 SQLALCHEMY_DATABASE_URI = 'postgresql://sign_language_api:flask123@localhost:5432/sign_language_api'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-IMAGE_PATH = ''
+IMAGE_PATH = '.'
 
 """Heroku database configuration."""
 try:
