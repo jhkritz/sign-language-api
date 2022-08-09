0c6301b618a076a2b17388d06707df8f79d24105
Keaton <23577983@sun.ac.za>
2022-08-09 13:04:32 +0200

    Added endpoints for creating single libraries and adding single signs. Also added axios function to addsign.vue (Still WIP)

--- frontend/src/views/addsign.vue
+++ frontend/src/views/addsign.vue
@@ -1,42 +1,25 @@
 <!-- References:https://www.digitalocean.com/community/tutorials/how-to-handle-file-uploads-in-vue-2 -->
 <template>
-    <v-form v-model="valid">
-      <v-container>
+  <v-form v-model="valid">
+    <v-container>
       <!-- UPLOAD SIGN -->
       <h1>Add sign</h1>
-       <div class="dropbox">
-         <input type="file" 
-         single
-         required
-         >
-       </div>  
+      <div class="dropbox">
+        <input type="file" single required>
+      </div>
       <!-- SIGN NAME -->
-      <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-       <v-text-field
-         v-model="signname"
-         label="Sign name"
-         :rules="signrules"
-         outlined
-         required
-       ></v-text-field>
+      <v-col cols="12" sm="6" md="3">
+        <v-text-field v-model="signname" label="Sign name" :rules="signrules" outlined required></v-text-field>
       </v-col>
-      <v-btn
-        depressed
-        @click="postInfo"
-        >
-      Submit
-      </v-btn> 
-      </v-container>   
-    </v-form>  
+      <v-btn depressed @click="postSign">
+        Submit
+      </v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-import axios from 'axios';
-export default ({
+export default {
   data: () => ({
     valid: false,
     signname: '',
@@ -44,46 +27,68 @@ export default ({
       v => !!v || 'Sign name is required'
     ]
   }
-  )
-})
+  ),
+
   methods: {
-    async postInfo (){
-      this.$axios.post('/library/addsign',{
-        signname: this.signname,
+    async postSign() {
+      var axios = require('axios');
+      var FormData = require('form-data');
+
+      var data = new FormData();
+      data.append('sign_name', this.signname);
+      //data.append('image_file', this.image);
+
+      var config = {
+        method: 'post',
+        url: 'http://localhost:5000/library/uploadsign',
+        data: data
+      };
+
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
         })
-      },
-    },
-  }
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+
+}
 </script>
 
 <!-- SASS styling -->
 <style lang="scss">
-  .dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: lightcyan;
-    color: dimgray;
-    padding: 10px 10px;
-    min-height: 200px; /* minimum height */
-    position: relative;
-    cursor: pointer;
-  }
-  
-  .input-file {
-    opacity: 0; /* invisible but it's there! */
-    width: 100%;
-    height: 200px;
-    position: absolute;
-    cursor: pointer;
-  }
-  
-  .dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
-  }
-  
-  .dropbox p {
-    font-size: 1.2em;
-    text-align: center;
-    padding: 50px 0;
-  }
+.dropbox {
+  outline: 2px dashed grey;
+  /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px;
+  /* minimum height */
+  position: relative;
+  cursor: pointer;
+}
+
+.input-file {
+  opacity: 0;
+  /* invisible but it's there! */
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background: lightblue;
+  /* when mouse over to the drop zone, change color */
+}
+
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
 </style>
