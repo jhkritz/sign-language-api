4c405bfa84331a81327bb803874e386f4fe5500f
Shabih, MH, Miss [24397644@sun.ac.za] <24397644@sun.ac.za>
2022-08-10 23:11:17 +0200

    Displayel of images in a library

--- frontend/src/views/library.vue
+++ frontend/src/views/library.vue
@@ -9,7 +9,32 @@
         Add sign 
         </v-btn>
         <v-row>
-        </v-row>
+    <v-col
+      v-for="n in 9"
+      :key="n"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      <v-img
+        :src="`http://images.agoramedia.com/wte3.0/gcms/baby-sign-launage-722x406.jpg?width=414=${n * 5 + 10}`"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
+  </v-row>
     </v-container>    
 </template>
 
