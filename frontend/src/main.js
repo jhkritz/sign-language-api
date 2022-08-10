867b29dad3ee2fff291e3d6230ec9013b3b1148a
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-08-11 01:59:30 +0200

    Added a route and a page to catch invalid URLs.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-//import router from './router'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import axios from 'axios'
@@ -9,52 +8,62 @@ import VueAuth from 'vue-auth'
 import home from './views/home'
 import addsign from './views/addsign'
 import library from './views/library'
+import PageNotFound from './views/PageNotFound';
+import UploadLibrary from './views/UploadLibrary';
 
 Vue.config.productionTip = false
 
 const routes = [
 
-  {
-    path: '/',
-    name: 'home',
-    component: home
-  },
-  {
-    path: '/library/:id',
-    name: 'library',
-    component: library,
-  },
-  {
-    path: '/addsign',
-    name: 'addsign',
-    component: addsign,
-  },
+	{
+		path: '/',
+		name: 'home',
+		component: home
+	},
+	{
+		path: '/library/upload',
+		component: UploadLibrary
+	},
+	{
+		path: '/library/:id',
+		name: 'library',
+		component: library,
+	},
+	{
+		path: '/addsign',
+		name: 'addsign',
+		component: addsign,
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		component: PageNotFound
+	},
 ]
 
 Vue.use(VueRouter)
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+	baseURL: 'http://127.0.0.1:8000/',
 })
 
 instance.interceptors.request.use(
-  config => {
-    let accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
-      config.headers = Object.assign({
-        Authorization: accessToken
-      }, config.headers);
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
+	config => {
+		let accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			config.headers = Object.assign({
+				Authorization: accessToken
+			}, config.headers);
+		}
+		return config;
+	},
+	error => {
+		return Promise.reject(error);
+	}
 );
 
 const router = new VueRouter({
-  mode: 'history',
-  routes,
+	mode: 'history',
+	routes,
 })
 
 Vue.prototype.$axios = instance
@@ -62,7 +71,7 @@ Vue.prototype.$axios = instance
 Vue.use(VueAuth)
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+	router,
+	vuetify,
+	render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/PageNotFound.vue
@@ -0,0 +1,5 @@
+<template>
+	<v-container>
+		<h1>404: Page not found</h1>
+	</v-container>
+</template>
