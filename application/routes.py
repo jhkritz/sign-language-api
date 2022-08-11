d9a11d0a9b3572ba15fd053787a5e0610a9ae911
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-08-11 18:57:19 +0200

    Added the client code related to sockets. The socket events aren't being handled.

--- application/routes.py
+++ application/routes.py
@@ -264,6 +264,7 @@ def classify_image():
 ####################################################################################################
 @socketio.on('connect')
 def test_connect():
+    print('emitting')
     emit('after_connect', "connected")
 
 
