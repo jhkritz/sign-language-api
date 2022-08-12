058bd004348fe491396ce351827264fce1cf7752
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-08-12 21:14:26 +0200

    Added a 'return home' button to the 404 page.

--- frontend/src/views/page-not-found.vue
+++ frontend/src/views/page-not-found.vue
@@ -1,5 +1,17 @@
 <template>
-	<v-container>
-		<h1>404: Page not found</h1>
-	</v-container>
+    <v-container>
+        <h1>404: Page not found</h1>
+        <v-btn @click.stop='navigateHome'>
+            Click here to return to the home page.
+        </v-btn>
+    </v-container>
 </template>
+<script>
+    export default {
+        methods: {
+            navigateHome() {
+                this.$router.push('/');
+            }
+        }
+    }
+</script>
