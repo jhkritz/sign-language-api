974370d7f84bb75bfb863b28cba9eb6b5fb84891
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-08-15 23:47:47 +0200

    More code related to the navigation bar.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <template>
     <v-app>
         <v-main>
+            <navBar app v-if="this.$router.currentRoute.path !== '/'" />
             <router-view />
         </v-main>
     </v-app>
 </template>
 
 <script>
+    import navBar from './components/navigation-bar';
     export default {
         name: "App",
         data: () => ({}),
-        components: {},
+        components: {
+            navBar
+        },
     };
 </script>
