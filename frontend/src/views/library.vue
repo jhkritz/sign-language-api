a29302a6f5f3fa52db651b393ed7989e317ae330
Shabih, MH, Miss [24397644@sun.ac.za] <24397644@sun.ac.za>
2022-08-18 17:33:19 +0200

    Cleaned up and somewhat styled code

--- frontend/src/views/library.vue
+++ frontend/src/views/library.vue
@@ -6,23 +6,26 @@
                     <v-data-table :headers="headers" :items="signs" class="elevation-1">
                         <template v-slot:top>
                             <v-toolbar flat>
-                                <v-toolbar-title>MY SIGNS</v-toolbar-title>
-                                <v-divider class="mx-4" inset vertical></v-divider>
-                                <v-spacer></v-spacer>
-                                <v-btn dark color="orange darken-4" class="mb-2" v-bind="attrs" v-on="on" @click="goto_addsign">
-                                    Add Sign
-                                </v-btn>
-                                <v-dialog v-model="dialogDelete" max-width="500px">
-                                    <v-card>
-                                        <v-card-title class="text-h5">Are you sure you want to delete this sign?</v-card-title>
-                                        <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="black" text @click="closeDelete">Cancel</v-btn>
-                                            <v-btn color="black" text @click="deleteItemConfirm">OK</v-btn>
-                                            <v-spacer></v-spacer>
-                                        </v-card-actions>
-                                    </v-card>
-                                </v-dialog>
+                                <v-toolbar-title>
+                                    MY SIGNS
+                                </v-toolbar-title>
+                                    <v-divider class="mx-4" inset vertical>
+                                    </v-divider>
+                                        <v-spacer></v-spacer>
+                                            <v-btn dark color="orange darken-4" class="mb-2" v-bind="attrs" v-on="on" @click="goto_addsign">
+                                                Add Sign
+                                            </v-btn>
+                                    <v-dialog v-model="dialogDelete" max-width="500px">
+                                        <v-card>
+                                            <v-card-title class="text-h5">Are you sure you want to delete this sign?</v-card-title>
+                                            <v-card-actions>
+                                                <v-spacer></v-spacer>
+                                                    <v-btn color="black" text @click="closeDelete">Cancel</v-btn>
+                                                        <v-btn color="black" text @click="deleteItemConfirm">OK</v-btn>
+                                                <v-spacer></v-spacer>
+                                            </v-card-actions>
+                                        </v-card>
+                                    </v-dialog>
                             </v-toolbar>
                         </template>
                         <template v-slot:item.actions="{ item }">
