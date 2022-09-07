86d8241c8d98a43c31badc55b5b99c8554caed72
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-09-07 02:16:14 +0200

    Updated server.js to fix a routing bug.

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,14 @@
+// Based on the code given in the Heroku Nodejs docs
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs#push-local-changes
+// And the README.md linked below
+// https://github.com/bripkens/connect-history-api-fallback
 const express = require('express')
 const path = require('path')
+const midware = require('connect-history-api-fallback');
 const PORT = process.env.PORT || 5000
 
 const expressApp = express();
+expressApp.use(midware);
 expressApp.use(express.static(path.join(__dirname, 'dist')));
 expressApp.get('*', (req, res) => res.sendFile(__dirname, '/dist/index.html'));
 expressApp.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-/*
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'ejs')
-	.get('/', (req, res) => res.render('pages/index'))
-	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-		 */
