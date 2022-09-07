cf03808b8eec4b3dcab75817c713255500db76b9
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-09-07 02:28:50 +0200

    Updated server.js

--- frontend/server.js
+++ frontend/server.js
@@ -8,7 +8,9 @@ const midware = require('connect-history-api-fallback');
 const PORT = process.env.PORT || 5000
 
 const expressApp = express();
-expressApp.use(midware);
+expressApp.use(midware({
+	index: '/index.html'
+}));
 expressApp.use(express.static(path.join(__dirname, 'dist')));
 expressApp.get('*', (req, res) => res.sendFile(__dirname, '/dist/index.html'));
 expressApp.listen(PORT, () => console.log(`Listening on port ${PORT}`));
