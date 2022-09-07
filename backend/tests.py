db0b322fd4d4f893944f0cf274d7aa3bee09b082
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-09-07 03:54:16 +0200

    Wrote a single test case for the CI pipeline.

---
+++ backend/tests.py
@@ -0,0 +1,20 @@
+import subprocess
+import nose2
+
+
+def log_process(proc):
+    print('returncode:\n' + str(proc.returncode))
+    print('stdout:\n' + str(proc.stdout.decode('utf-8')))
+    print('stderr:\n' + str(proc.stderr.decode('utf-8')))
+
+
+def test_get_libraries():
+    cmd = "curl --location --request GET 'http://127.0.0.1:5000/libraries/getall'"
+    completed = subprocess.run(cmd, capture_output=True, shell=True)
+    log_process(completed)
+    assert completed.returncode == 0
+    assert completed.stdout.decode('utf-8').find('libraries') >= 0
+
+
+if __name__ == '__main__':
+    nose2.main()
