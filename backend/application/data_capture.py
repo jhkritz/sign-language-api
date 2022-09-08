201459dfa3b95399d445e987ab5c519d1eab76d1
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-09-09 00:58:59 +0200

    Updated data_capture.py so that it captures many images of the same sign.

--- backend/application/data_capture.py
+++ backend/application/data_capture.py
@@ -2,6 +2,7 @@ import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 import cv2 as cv
 import sys
+import os
 
 """
 Note: this code is inspired by the following video, and it is only for getting initial test data.
@@ -14,9 +15,10 @@ def get_signs(filename):
     offset = 30
     hand_detector = HandDetector(maxHands=1)
     capture = cv.VideoCapture(0)
+    i = 0
     while True:
         s, img = capture.read()
-        cv.imwrite(filename, img)
+        cv.imwrite(filename + str(i) + '.png', img)
         hands, hands_img = hand_detector.findHands(img)
         try:
             x, y, w, h = hands[0]['bbox']
@@ -24,15 +26,17 @@ def get_signs(filename):
             cropped = cv.resize(cropped, desired_shape)
             cv.imshow('cropped', cropped)
             cv.waitKey(1)
+            i += 1
             # Write the unprocessed image.
             #cv.imwrite(filename, img)
             #cv.imwrite(filename, cropped)
         except Exception as e:
             print(e)
+            os.remove(filename + str(i) + '.png')
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('Usage <filename>')
     else:
-        get_signs(sys.argv[1] + '.jpg')
+        get_signs(sys.argv[1])
