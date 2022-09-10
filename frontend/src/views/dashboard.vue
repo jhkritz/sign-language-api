5be32d3f6e59c28cbddf7d2a59d04bd9686ad7b2
Shabih, MH, Miss [24397644@sun.ac.za] <24397644@sun.ac.za>
2022-09-10 19:17:56 +0200

    Actual dashboard being implemented

---
+++ frontend/src/views/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+<v-app-bar
+  elevation="4"
+  
+>
+<v-spacer></v-spacer>
+      <v-btn
+        color="black"
+        class="mr-4 white--text"
+      >
+        Register
+      </v-btn>
+
+      <v-btn
+        color="black"
+        class="mr-4 white--text"
+      >
+        Login
+      </v-btn>
+</v-app-bar>
+    
+</template>
