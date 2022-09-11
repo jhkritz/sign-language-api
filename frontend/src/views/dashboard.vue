8511dd613b62c2e3ca8eadaeb2a6eeee87b885ed
Shabih, MH, Miss [24397644@sun.ac.za] <24397644@sun.ac.za>
2022-09-11 13:27:01 +0200

    Buttons navigate to the correct pages

--- frontend/src/views/dashboard.vue
+++ frontend/src/views/dashboard.vue
@@ -7,6 +7,7 @@
       <v-btn
         color="black"
         class="mr-4 white--text"
+        @click="goToRegister()"
       >
         Register
       </v-btn>
@@ -14,9 +15,28 @@
       <v-btn
         color="black"
         class="mr-4 white--text"
+        @click="goToLogin()"
       >
         Login
       </v-btn>
 </v-app-bar>
     
 </template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+  methods:{
+  goToRegister(){
+    this.$router.push('/register');
+  },
+  goToLogin(){
+    this.$router.push('/login');
+  },
+  },
+})
+</script>
