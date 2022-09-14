ffdcbcef375180e14d10bbdda8821328cc8d4046
Kritzinger, JH, Mnr [21616442@sun.ac.za] <21616442@sun.ac.za>
2022-09-14 15:17:45 +0200

    Updated home screen layout

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,15 +1,15 @@
 <style>
 .bg {
-  position: fixed;
+  position: absolute;
   top: 0px;
   left: 0;
   z-index: 999;
   width: 100%;
-  height: 100vh;
-  text-align: center;
+  height: 180vh;
+  overflow: scroll;
 
   background-image: url('../assets/bg-poly.svg');
-  background-attachment: scroll;
+  background-attachment: fixed;
   background-size: cover;
   background-position: left, top;
 }
@@ -21,7 +21,7 @@
 
 .main-text {
   text-align: left;
-  margin-top: 12vh;
+  margin-top: 3vh;
   margin-left: 5vh;
   color: white;
   font-family: Arial, Helvetica, sans-serif;
@@ -42,12 +42,33 @@
   font-size: 1.5em;
   width: 45vw;
 }
+
+.features {
+  width: 100%;
+  align-content: space-between;
+  display: flex;
+  margin-top: 5vh;
+  margin-left: 5vh;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 2vw;
+}
+
+.frosted {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #4D949B;
+  max-width: 20em;
+  max-height: 20em;
+  min-width: 20em;
+  min-height: 5em;
+  backdrop-filter: blur(40px) hue-rotate(0deg);
+}
 </style>
 
 <template>
   <div class="bg">
     <v-app-bar elevation="0" color=transparent class="bar">
-      <v-toolbar-title class="white--text">Sign Language API</v-toolbar-title>
+      <!--v-toolbar-title class="white--text">Sign Language API</v-toolbar-title-->
       <v-spacer></v-spacer>
       <v-btn color=#17252A class="mr-4 white--text" @click="goToRegister()">
         Register
@@ -72,6 +93,54 @@
       <v-btn rounded x-large color=#17252A class="mr-4 white--text" @click="goToLogin()">
         Login
       </v-btn>
+    </div>
+
+    <div class="features">
+
+      <v-card color=#4D949B dark shaped class="frosted" elevation="15">
+        <v-icon large style="padding:0.8vw">mdi-folder-multiple-image</v-icon>
+        <v-card-title class="text-h5">
+          Custom image libraries
+        </v-card-title>
+
+        <v-card-subtitle>Create custom image libraries that are trained on sign data you supply.
+        </v-card-subtitle>
+      </v-card>
+
+      <v-card color=#4D949B dark shaped class="frosted" elevation="15">
+        <v-icon large style="padding:0.8vw">mdi-hand-wave</v-icon>
+        <v-card-title class="text-h5">
+          Train individual signs
+        </v-card-title>
+
+        <v-card-subtitle>Capture and label hand signs for training straight from your webcam!
+        </v-card-subtitle>
+      </v-card>
+
+    </div>
+
+    <div class="features">
+
+      <v-card color=#4D949B dark shaped class="frosted" elevation="15">
+        <v-icon large style="padding:0.8vw">mdi-zip-box</v-icon>
+        <v-card-title class="text-h5">
+          Bulk file uploads
+        </v-card-title>
+
+        <v-card-subtitle>Bulk upload a labelled image dataset for training.
+        </v-card-subtitle>
+      </v-card>
+
+      <v-card color=#4D949B dark shaped class="frosted" elevation="15">
+        <v-icon large style="padding:0.8vw">mdi-code-tags</v-icon>
+        <v-card-title class="text-h5">
+          API usage
+        </v-card-title>
+
+        <v-card-subtitle>Apply for an API key to use your trained datasets in your own projects.
+        </v-card-subtitle>
+      </v-card>
+
     </div>
 
 
