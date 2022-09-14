785d3f1e2114670385612798b10342660b0e7bd1
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-09-14 19:45:31 +0200

    Updated the code so that users are immediately logged in after registering.

--- backend/application/login_routes.py
+++ backend/application/login_routes.py
@@ -4,29 +4,30 @@ from .models import User, APIKeys
 import hashlib
 import random
 import uuid
-from flask_jwt_extended import (create_access_token, 
-create_refresh_token, 
-set_access_cookies,
-set_refresh_cookies,
-jwt_required,
-unset_jwt_cookies,
-get_jwt_identity
-)
+from flask_jwt_extended import (create_access_token,
+                                create_refresh_token,
+                                set_access_cookies,
+                                set_refresh_cookies,
+                                jwt_required,
+                                unset_jwt_cookies,
+                                get_jwt_identity
+                                )
+
 
 @app.route('/login', methods=['POST'])
 def login():
     email = request.json.get('email')
     existinguser = User.query.filter_by(email=email).first()
     if not existinguser:
-        return {'message':'Unkown user'}, 400
+        return {'message': 'Unkown user'}, 400
     password = request.json.get('password')
-    password_hash = hashlib.sha512(str(password).encode("utf-8") ).hexdigest()
+    password_hash = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
     if not existinguser.pass_hash == password_hash:
-        return {'message':'Incorrect password'}, 400
+        return {'message': 'Incorrect password'}, 400
 
     access_token = create_access_token(identity=existinguser.id)
     refresh_token = create_refresh_token(identity=existinguser.id)
-    #return auth and token in json
+    # return auth and token in json
     return {'access': access_token, 'refresh': refresh_token}, 200
 
 
@@ -37,7 +38,7 @@ def register():
     if existinguser:
         return {'message': 'User exists'}, 400
     password = request.json.get('password')
-    password_hash = hashlib.sha512(str(password).encode("utf-8") ).hexdigest()
+    password_hash = hashlib.sha512(str(password).encode("utf-8")).hexdigest()
     user = User(email=email, pass_hash=password_hash)
     db.session.add(user)
     db.session.commit()
@@ -47,20 +48,19 @@ def register():
     access_token = create_access_token(identity=user.id)
     refresh_token = create_refresh_token(identity=user.id)
 
-    response = jsonify({'api_key':key})
+    return {'api_key': key, 'access': access_token, 'refresh': refresh_token}
 
     #set_access_cookies(response, access_token)
     #set_refresh_cookies(response, refresh_token)
-
-    return response
 
 
 @app.route('/logout', methods=['POST'])
 @jwt_required()
 def logout():
-    response = jsonify({'message':'Success'})
+    response = jsonify({'message': 'Success'})
     unset_jwt_cookies(response)
     return response
+
 
 @app.route('/refresh', methods=['GET'])
 @jwt_required(refresh=True)
@@ -70,9 +70,7 @@ def refreshtoken():
     access_token = create_access_token(identity=user.id)
     refresh_token = create_refresh_token(identity=user.id)
     response = jsonify()
-    return {'access':access_token, 'refresh': refresh_token}
-
-
+    return {'access': access_token, 'refresh': refresh_token}
 
 
 @app.route('/api/resetapikey', methods=['GET'])
@@ -82,33 +80,36 @@ def resetapikey():
     return generateapikey(userid)
 
 
-#create api key and store hash in DB
-#return unhashed key
+# create api key and store hash in DB
+# return unhashed key
 def generateapikey(id):
 
-    #check for existing key and remove it
-    if not (User.query.filter_by(id = id).first()):
+    # check for existing key and remove it
+    if not (User.query.filter_by(id=id).first()):
         return "0"
-    existingkey = APIKeys.query.filter_by(userid = id).delete()
+    existingkey = APIKeys.query.filter_by(userid=id).delete()
 
     key = str(id) + "." + str(uuid.uuid4())
-    key_hash = hashlib.sha512(str(key).encode("utf-8") ).hexdigest()
+    key_hash = hashlib.sha512(str(key).encode("utf-8")).hexdigest()
     apikey = APIKeys(userid=id, api_key_hash=key_hash)
     db.session.add(apikey)
     db.session.commit()
 
     return key
 
-#verify key and return user id. return 0 if failed to verify
+# verify key and return user id. return 0 if failed to verify
+
+
 def verifykey(api_key):
-    hash = hashlib.sha512(str(api_key).encode("utf-8") ).hexdigest()
+    hash = hashlib.sha512(str(api_key).encode("utf-8")).hexdigest()
     keys = APIKeys.query.all()
-    
+
     userid = 0
     for key in keys:
         if key.api_key_hash == hash:
             userid = key.userid
     return str(userid)
+
 
 @app.route('/api/testkey', methods=['POST'])
 def testkey():
--- frontend/src/views/register.vue
+++ frontend/src/views/register.vue
@@ -1,100 +1,68 @@
 <template>
     <v-app>
-      <div>
-      <!--Navigation bar-->  
-      <v-app-bar
-        elevation="0"
-        color=#3AAFA9
-      >
-      <v-btn icon color= "white" id='homeButton' to='/'>
-            <v-icon>mdi-home</v-icon>
-        </v-btn>
-      <v-appbar-title class = "white--text">Sign Language API</v-appbar-title>
-      </v-app-bar>
-    </div>
-      <!--Registration form-->
-      <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-    >
-      <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-      ></v-text-field>
-  
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-  
-      <v-text-field
-      v-model="password"
-      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-      :rules="passwordRules"
-      :type="show1 ? 'text' : 'password'"
-      name="input-10-1"
-      label="Password"
-      counter
-      @click:append="show1 = !show1"
-      required
-    ></v-text-field>
-  
-      <v-btn
-        :disabled="!valid"
-        color=#17252A
-        class="mr-4 white--text"
-        @click="postInfo()"
-      >
-        Register
-      </v-btn>
-  
-    </v-form>
-</v-app>
-  </template>
+        <div>
+            <!--Navigation bar-->
+            <v-app-bar elevation="0" color=#3AAFA9>
+                <v-btn icon color="white" id='homeButton' to='/'>
+                    <v-icon>mdi-home</v-icon>
+                </v-btn>
+                <v-appbar-title class="white--text">Sign Language API</v-appbar-title>
+            </v-app-bar>
+        </div>
+        <!--Registration form-->
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+            <v-text-field v-model="password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="passwordRules" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" counter @click:append="show1 = !show1" required></v-text-field>
+
+            <v-btn :disabled="!valid" color=#17252A class="mr-4 white--text" @click="postInfo()">
+                Register
+            </v-btn>
+
+        </v-form>
+    </v-app>
+</template>
 
 <script>
     import {
         sharedState
     } from '../SharedState';
-    
+
     import {
         baseUrl
     } from '../BaseRequestUrl';
     const axios = require('axios');
     export default {
-      data: () => ({
-        valid: true,
-        name: '',
-        nameRules: [
-          v => !!v || 'Name is required',
-          v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        ],
-        email: '',
-        emailRules: [
-          v => !!v || 'E-mail is required',
-          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        ],
-        password: "",
-        passwordRules: [
-        (v) => !!v || "Password is required",
-        (v) => v.length <= 20 || "Password must be less than 20 characters",
-        (v) => v.length >= 5 || "Password must be more than 4 characters"],
-      }),
-  
-      methods: {
-        validate () {
-          this.$refs.form.validate()
-        },
-        async postInfo() {
-                const data =  {
-                  email: this.email,
-                  password: this.password
+        data: () => ({
+            valid: true,
+            name: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            password: "",
+            passwordRules: [
+                (v) => !!v || "Password is required",
+                (v) => v.length <= 20 || "Password must be less than 20 characters",
+                (v) => v.length >= 5 || "Password must be more than 4 characters"
+            ],
+        }),
+
+        methods: {
+            validate() {
+                this.$refs.form.validate()
+            },
+            async postInfo() {
+                const data = {
+                    email: this.email,
+                    password: this.password
                 };
                 const config = {
                     method: 'post',
@@ -110,11 +78,12 @@
                         console.log(res.data);
                         console.log(res.headers);
                         console.log(res.data['api_key']);
+                        localStorage.setItem('access_token', res.data['access'])
+                        localStorage.setItem('refresh_token', res.data['refresh'])
                         sharedState.setAPIkey(res.data['api_key']);
                         this.$router.push(`/API?API_key=${res.data['api_key']}`);
-                    }
-                    else {
-                      alert(res.data)
+                    } else {
+                        alert(res.data)
                     }
                 } catch (err) {
                     console.error(err);
@@ -122,7 +91,7 @@
                 }
             },
 
-        
-      },
+
+        },
     }
-  </script>
+</script>
