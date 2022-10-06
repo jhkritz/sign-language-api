9ddf46636857558c394c35827f9467658b425b27
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-10-06 17:44:04 +0200

    Updated the test script so that it acquires a new access token every time it's run.

--- backend/tests.py
+++ backend/tests.py
@@ -1,5 +1,9 @@
 import subprocess
 import nose2
+import json
+
+
+access_token = ""
 
 
 def log_process(proc):
@@ -8,14 +12,25 @@ def log_process(proc):
     print('stderr:\n' + str(proc.stderr.decode('utf-8')))
 
 
+def get_access_token():
+    cmd = "curl --location --request POST 'http://127.0.0.1:5000/login'\
+            --header 'Content-Type: application/json' \
+            --data-raw '{\"email\": \"ci@cd.com\", \"password\": \"continuous\"}'"
+    completed = subprocess.run(cmd, capture_output=True, shell=True)
+    output = json.loads(completed.stdout.decode('utf-8'))
+    return output['access']
+
+
 def test_get_libraries():
-    cmd = "curl --location --request GET 'http://127.0.0.1:5000/libraries/getall' --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY2MzIwNDEyMCwianRpIjoiY2ViNzc4YWUtYzg4NC00N2IzLWEwZTUtMTE2ZWM1YzAwYmNmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjYzMjA0MTIwLCJleHAiOjE2NjMyMDUwMjB9.lCA-yNR9SxrrZZaOs-ItU-jCvz7wINySCoEiBLhgyuY'"
+    cmd = "curl --location --request GET 'http://127.0.0.1:5000/libraries/getall'\
+            --header 'Authorization: Bearer {}'".format(access_token)
     completed = subprocess.run(cmd, capture_output=True, shell=True)
     log_process(completed)
     assert completed.returncode == 0
     output = completed.stdout.decode('utf-8')
-    assert output.find('libraries') >= 0 or output.find('expired') >= 0
+    assert output.find('libraries') >= 0
 
 
 if __name__ == '__main__':
+    access_token = get_access_token()
     nose2.main()
