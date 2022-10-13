cd10b39c29127a328b1058fa5a7035a0b83d7bac
Martin, RHJ, Mr [23271310@sun.ac.za] <23271310@sun.ac.za>
2022-10-13 21:36:15 +0200

    Removed the pencil icon.

--- frontend/src/views/library.vue
+++ frontend/src/views/library.vue
@@ -85,7 +85,6 @@
                             </template>
                             <template v-slot:item.actions="{ item }">
                                 <v-icon small class="mr-2" @click="viewSign(item)"> mdi-eye </v-icon>
-                                <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
                                 <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
                             </template>
                             <template v-slot:no-data>
